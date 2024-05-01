e0ba38ea7a26949222d36db7c4267197347897ac
Gilles <quodbach.gilles@gmail.com>
2024-05-01 09:23:33 +0200

    bugfix

--- src/main/java/fr/fms/dao/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.fms.dao;
-
-import fr.fms.entities.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
